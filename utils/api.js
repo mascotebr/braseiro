b0121fc6cc6ab4269d8f3d5b22645dd35a3e52fa
Gabriel Conti <ga_con@hotmail.com>
2025-12-16 14:41:38 -0300

    adicionado e encapsulado apiFunction para apis e tratamento de methods em rotas apis e em `api/v1/migrations`

--- utils/api.js
+++ utils/api.js
@@ -16,7 +16,28 @@ async function get(url) {
 
 async function post(url, body) {
   const baseUrl = process.env.BASE_URL_API
-  return await fetch(`${baseUrl}${url}`, { method: 'POST' })
+  return await fetch(`${baseUrl}${url}`, { method: 'POST', body: body })
 }
 
-export { createApiRoute, get, post }
+async function apiFunction(req, res, methods) {
+  //Se o metodo não for declarado volta 405
+  if (!Object.keys(methods).includes(req.method)) {
+    return res.status(405).end()
+  }
+  let configParams
+  if (Object.keys(methods).includes('CONFIG')) {
+    configParams = await methods['CONFIG']()
+  }
+
+  try {
+    await methods[req.method](configParams)
+  } catch (error) {
+    console.log(error)
+    return error
+  } finally {
+    if (Object.keys(methods).includes('DISMISS')) {
+      await methods['DISMISS'](configParams)
+    }
+  }
+}
+export { createApiRoute, get, post, apiFunction }
