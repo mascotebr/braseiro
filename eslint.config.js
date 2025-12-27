a562efceaa5ea844cb76b2285c2d9608f2b69505
Mascotebr <ga_con@hotmail.com>
2025-12-26 23:18:21 -0300

    adds plugins to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
 import js from '@eslint/js'
+import jest from 'eslint-plugin-jest'
 export default defineConfig([
+  ...nextVitals,
   {
     files: ['**/*.js'],
-    extends: ['js/recommended'],
-    plugins: { js },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'off',
-    },
+    plugins: { js, jest },
+    extends: ['js/recommended', 'jest/recommended', 'prettier'],
   },
   globalIgnores(['node_modules/*', '.next/*']),
 ])
