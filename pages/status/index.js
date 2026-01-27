6c50e437a21352a2578bf1ef635a78521fbfb96e
Mascotebr <ga_con@hotmail.com>
2026-01-26 22:43:33 -0300

    fix: fix import methods in 'pages/status'

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,5 +1,5 @@
 import useSWR from 'swr'
-import { get } from 'infra/methods'
+import methods from 'infra/methods'
 export default function StatusPage() {
   return (
     <>
@@ -11,7 +11,7 @@ export default function StatusPage() {
 }
 
 async function getStatus(key) {
-  const response = await get(key)
+  const response = await methods.get(key)
   return await response.json()
 }
 function UpdateAt() {
